2bdfd8d1ff1da7b3d26ff31e1dedb0629cf75104
sonsu55 <sonsu9155@hotmail.com>
2019-07-07 11:42:25 +0800

    no message

--- config/admin.php
+++ config/admin.php
@@ -11,7 +11,7 @@ return [
     | login page.
     |
     */
-    'name' => 'Laravel-admin',
+    'name' => 'LiveBroadCast-admin',
 
     /*
     |--------------------------------------------------------------------------
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo' => '<b>Laravel</b> admin',
+    'logo' => '<b>LiveBroadCast</b> admin',
 
     
 
@@ -36,7 +36,7 @@ return [
     | '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo-mini' => '<b>La</b>',
+    'logo-mini' => '<b>LB</b>',
 
     /*
     |--------------------------------------------------------------------------
